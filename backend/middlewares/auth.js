3ec85b7222f28e86421f4affe4ae17712be49f22
Ïgor Zakharov <Igor1994294@yandex.ru>
2021-11-25 13:29:18 +0300

    feat: change auth

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,22 +1,21 @@
 const jwt = require('jsonwebtoken');
 const AuthError = require('../errors/auth-error');
+const { authRequired } = require('../constants');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
+const { JWT_SECRET } = require('../config');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new AuthError('Необходима авторизация');
+    throw new AuthError(authRequired);
   }
-
   const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (e) {
-    throw new AuthError('Необходима авторизация');
+    throw new AuthError(authRequired);
   }
 
   req.user = payload;
